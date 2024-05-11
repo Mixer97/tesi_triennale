3a93fcc48c979d68879670d73f5e21b24118e0de
Mixer97 <169163595+Mixer97@users.noreply.github.com>
2024-05-11 11:38:11 +0200

    Update main_Client_Sinc_TCP.py

--- Codice_Progetto/main_Client_Sinc_TCP.py
+++ Codice_Progetto/main_Client_Sinc_TCP.py
@@ -9,7 +9,7 @@ def main(no):
     elif(no==2): # Lettura mV
         while(True):
             res=client_Sinc_TCP.WORKING.read_holding_registers_mV()        # Work in progress
-            # print("Risultato: "  + str(res))
+            print("Risultato: "  + str(res))
             
     elif(no==3): # Lettura SR
         client_Sinc_TCP.TESTING.read_status_register()
@@ -25,7 +25,7 @@ def main(no):
         client_Sinc_TCP.WORKING.test_conversione(40000) #wORKING
 
     elif(no==7):
-        client_Sinc_TCP.WORKING.teoretical_equalization([0,0,2,0]) #WORKING
+        # client_Sinc_TCP.WORKING.teoretical_equalization([0,0,2,0]) #WORKING
         client_Sinc_TCP.WORKING.tara_reset() #WORKING
 
     elif(no==8):
@@ -39,7 +39,11 @@ def main(no):
 
     elif(no==10):
         client_Sinc_TCP.WORKING.write_channels_active(4) # Valore da inserire in base 10 (i canali sono da six a dex in binario)
-
+        
+    elif(no==11):
+        risultatimV=client_Sinc_TCP.client.read_holding_registers(address=50, count=24, slave=client_Sinc_TCP.SLAVE.ID)
+        print(risultatimV.registers)
+        
     elif(no==666):
         while True:
             client_Sinc_TCP.TESTING.read_HIGHRES_divisions() #INCASINA TUTTO, CAPIRE PERCHE'!
@@ -47,4 +51,4 @@ def main(no):
     else:
         print("Out of bounds.")
 
-main(2)
+main(11)
