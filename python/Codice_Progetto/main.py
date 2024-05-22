8809d4b12c3dbfeb79c6d02754c6283bd1c5a159
Mixer97 <169163595+Mixer97@users.noreply.github.com>
2024-05-22 17:17:20 +0200

    reset finale

---
+++ python/Codice_Progetto/main.py
@@ -0,0 +1,26 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from QT_Creator_dell_App_4_ui import Ui_MainWindow  # Import the generated UI class
+import logger_CSV_Inserisco_direttamente
+from threading import Thread
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # Create an instance of the generated UI class
+        self.ui = Ui_MainWindow()
+        # Setup the user interface
+        self.ui.setupUi(self)
+        # Create and start a thread for the logger
+        logger_thread = Thread(target=run_logger)
+        logger_thread.start()     
+
+def run_logger():
+    logger_CSV_Inserisco_direttamente.Logger.logger(nome_CSV= "test")  
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+    
