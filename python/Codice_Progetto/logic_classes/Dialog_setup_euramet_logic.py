892b60555bb3a218c683556270c451ccd526f67c
Mixer97 <169163595+Mixer97@users.noreply.github.com>
2024-08-06 10:52:52 +0200

    Update Dialog_setup_euramet_logic.py

--- python/Codice_Progetto/logic_classes/Dialog_setup_euramet_logic.py
+++ python/Codice_Progetto/logic_classes/Dialog_setup_euramet_logic.py
@@ -62,6 +62,8 @@ class Euramet_window(QDialog):
         self.ui.horizontalSlider_stabilita_verde.sliderReleased.connect(self.update_valori_sliders)
         self.ui.horizontalSlider_varianza_giallo.sliderReleased.connect(self.update_valori_sliders)
         self.ui.horizontalSlider_varianza_verde.sliderReleased.connect(self.update_valori_sliders)
+        
+        self.update_valori_sliders()  # necessario per avere i valori inizializzati delle label
 
 
     # usato per inizializzare il secondo quadrante e terminare alla fine del 2 quadrante
